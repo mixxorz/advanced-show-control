82a0226a5c5ba79c61bce0c9340bf568b8249e92
Mitchel Cabuloy <mixxorz@gmail.com>
2026-06-10 16:00:11 +0800

    fix: label link dca channels

--- ui/src/format.ts
+++ ui/src/format.ts
@@ -13,12 +13,13 @@ export function channelDisplayGroup(group: number) {
   if (group === 1) return "Groups";
   if (group === 2) return "Aux";
   if (group === 6) return "Matrix";
+  if (group === 12) return "Link/DCAs";
   if ([3, 4, 5, 7, 8].includes(group)) return "Masters";
   return "Unknown";
 }
 
 export function channelDisplayGroupOrder(groupName: string) {
-  return ["Inputs", "Groups", "Aux", "Matrix", "Masters", "Unknown"].indexOf(groupName);
+  return ["Inputs", "Groups", "Aux", "Matrix", "Masters", "Link/DCAs", "Unknown"].indexOf(groupName);
 }
 
 export function channelButtonLabel(group: number, channel: number) {
