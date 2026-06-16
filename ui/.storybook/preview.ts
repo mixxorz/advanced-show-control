3146ad8cf2c742375bbda163cafad292bf1c71f2
Mitchel Cabuloy <mixxorz@gmail.com>
2026-06-17 00:44:49 +0800

    chore: configure storybook

---
+++ ui/.storybook/preview.ts
@@ -0,0 +1,22 @@
+import type { Preview } from "@storybook/react-vite";
+import "../src/index.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: "app",
+      values: [
+        { name: "app", value: "#020617" },
+        { name: "panel", value: "#0f172a" },
+      ],
+    },
+  },
+};
+
+export default preview;
