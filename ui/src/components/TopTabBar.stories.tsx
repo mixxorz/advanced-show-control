0d36dd6ca523b0827e02bf861130fac2e2d27385
Mitchel Cabuloy <mixxorz@gmail.com>
2026-06-18 19:33:04 +0800

    test: make safe toggle interactive in story

--- ui/src/components/TopTabBar.stories.tsx
+++ ui/src/components/TopTabBar.stories.tsx
@@ -1,10 +1,31 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { useState } from "react";
 import {
   connectedAppState,
   discoveringAppState,
 } from "../storybook/mockAppState";
 import { MockAppProviders } from "../storybook/MockAppProviders";
+import type { AppViewState } from "../types";
 import { TopTabBar } from "./TopTabBar";
+
+function StatefulTopTabBarStory(props: {
+  args: React.ComponentProps<typeof TopTabBar>;
+  initialAppState: AppViewState;
+}) {
+  const [appState, setAppState] = useState(props.initialAppState);
+
+  return (
+    <MockAppProviders
+      appState={appState}
+      commands={{
+        toggleLockout: () =>
+          setAppState((state) => ({ ...state, lockout: !state.lockout })),
+      }}
+    >
+      <TopTabBar {...props.args} />
+    </MockAppProviders>
+  );
+}
 
 const meta: Meta<typeof TopTabBar> = {
   title: "Shell/TopTabBar",
@@ -18,9 +39,7 @@ const meta: Meta<typeof TopTabBar> = {
     onSelectTab: () => {},
   },
   render: (args) => (
-    <MockAppProviders appState={connectedAppState}>
-      <TopTabBar {...args} />
-    </MockAppProviders>
+    <StatefulTopTabBarStory args={args} initialAppState={connectedAppState} />
   ),
 };
 
@@ -38,34 +57,30 @@ export const Logs: Story = {
 
 export const Offline: Story = {
   render: (args) => (
-    <MockAppProviders appState={discoveringAppState}>
-      <TopTabBar {...args} />
-    </MockAppProviders>
+    <StatefulTopTabBarStory args={args} initialAppState={discoveringAppState} />
   ),
 };
 
 export const Connecting: Story = {
   render: (args) => (
-    <MockAppProviders
-      appState={{
+    <StatefulTopTabBarStory
+      args={args}
+      initialAppState={{
         ...discoveringAppState,
         connection: "connecting",
       }}
-    >
-      <TopTabBar {...args} />
-    </MockAppProviders>
+    />
   ),
 };
 
 export const SafeActive: Story = {
   render: (args) => (
-    <MockAppProviders
-      appState={{
+    <StatefulTopTabBarStory
+      args={args}
+      initialAppState={{
         ...connectedAppState,
         lockout: true,
       }}
-    >
-      <TopTabBar {...args} />
-    </MockAppProviders>
+    />
   ),
 };
