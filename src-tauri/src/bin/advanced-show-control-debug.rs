6fa914ee90e9b7fbcae11467e7d72d6d66e1c4e1
Mitchel Cabuloy <mixxorz@gmail.com>
2026-07-04 21:09:09 +0800

    fix: keep debug app out of release build

--- src-tauri/src/bin/advanced-show-control-debug.rs
+++ src-tauri/src/bin/advanced-show-control-debug.rs
@@ -1,5 +1,11 @@
+#[cfg(debug_assertions)]
 fn main() {
     advanced_show_control::ui::debug::build_debug_app()
         .run(tauri::generate_context!("tauri.debug.conf.json"))
         .expect("failed to run Advanced Show Control Debug");
+}
+
+#[cfg(not(debug_assertions))]
+fn main() {
+    panic!("advanced-show-control-debug is only available in debug builds");
 }
